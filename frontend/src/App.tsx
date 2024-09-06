cd2ae8f68fdaf7f76658202af5e8eaec341f647c
Ole Anders <ole.nodland@bouvet.no>
2024-09-06 08:38:03 +0200

    Add image to the page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,6 @@ import { ChangeOrderPage } from './pages/ChangeOrder'
 import { NewOrderPage } from './pages/NewOrder'
 
 
-
 function App() {
   
   const [state, setState] = useState<StateMachine>(StateMachine.HomePage)
@@ -34,7 +33,10 @@ function App() {
   return (
     <div className='flex justify-center w-full'>
       <div className='flex gap-8 flex-col mt-16 m-6 text-center'>
-        <Typography variant='h1' className='mb-10'>Bike Repair</Typography>
+        <div className='mb-6 flex gap-4'>
+          <img src='/bikeRepairLogo.png' alt="Bike repair logo" width={50} height={50}/>
+          <Typography variant='h1'>Bike Repair</Typography>
+        </div>
         <Typography>Do you need a repair of your Bike, then you hav come to the right place </Typography>
         <div className='h-[2px] bg-slate-900'></div>
         {StateMachinePageMap[state]}
