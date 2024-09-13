e5ac03e3800d5e81c5dba78a5a3b807df77943ca
Ole Anders <ole.nodland@bouvet.no>
2024-09-13 12:33:34 +0200

    Working set up for sql database

--- deployment/README.md
+++ deployment/README.md
@@ -22,9 +22,31 @@ az deployment group create
 ./pushImage.sh
 ```
 
-## Spin up  det docker containers
+## Set up database
+* Set up a SQL database whit this criterial [this](https://learn.microsoft.com/en-us/azure/azure-sql/database/azure-sql-dotnet-entity-framework-core-quickstart?view=azuresql&tabs=dotnet-cli%2Cservice-connector%2Cportal) mal
+    * Server Name: bikedbserver
+    * Database Name: BikeRepairDb
+* run from main path:
+```bash
+cd '\efBike\
+dotnet ef migrations add InitialCreate
+dotnet ef database update
+```
+
+## Spin up frontend docker container
 ```bash
 az deployment group create
   --resource-group BikeRepair-OleAnders
   --template-file container-instance.bicep
+```
+
+## spin up Web api:
+* 
+
+
+
+## Nice code snippets
+Getting the user name and password for admin in the acr
+```bash
+az acr credential show --name <yourACRName>
 ```
--- efBike/Data/BikeRepairContext.cs
+++ efBike/Data/BikeRepairContext.cs
@@ -1,24 +1,41 @@
 using efBike.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace efBike
 {
-    public class ModelsContext: DbContext
+    public class BikeDbContext: DbContext
     {
-        // public BikeDbContext(DbContextOptions<BikeDbContext> options)
-        //     : base(options)
-        // {}
+        //Error when added
+        // public BikeDbContext(DbContextOptions<BikeDbContext> options): base(options)
+        // {
+
+        // }
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+        }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost,1433;Database=DB;User Id=sa;Password=OEFei394fnrfnr3490t!foefk;TrustServerCertificate=True;");
-                //Docker comand: docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=OEFei394fnrfnr3490t!foefk' -p 1433:1433 --name DB -d mcr.microsoft.com/mssql/server
+                // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            
+                //Local
+                // optionsBuilder.UseSqlServer("Server=localhost,1433;Database=DB;User Id=sa;Password=OEFei394fnrfnr3490t!foefk;TrustServerCertificate=True;");
+                
+                //Azure
+                optionsBuilder.UseSqlServer("Server=tcp:bikedbserver.database.windows.net,1433;Initial Catalog=bikerepairdb;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication='Active Directory Default'");
+                
+                //Docker command: 
+                //docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=OEFei394fnrfnr3490t!foefk' -p 1433:1433 --name DB -d mcr.microsoft.com/mssql/server
             }
         }
 
